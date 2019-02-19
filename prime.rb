9964eb661fa26094668fbfde388e94b3a420a37c
David Stewart <davidstewart@gmail.com>
2019-02-19 05:19:31 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,9 +1,9 @@
 
 
-def prime?(x)
-  first = 2
+def prime?(number)
+  start = 2
   if number > 1
-    range = (first..x-1).to_a
+    range = (start..number-1).to_a
     range.none? do |num_to_test| #none of the numbers should return true for none? to return true. If none of the numbers
                                 # return true, then they are not divisible by the number and the number is prime
       number % num_to_test == 0
